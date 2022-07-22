7e85654955ad443c164099dc6c27ae3852b314d9
Pratik3003 <psingh302000@gmail.com>
2022-07-22 20:38:43 +0530

    dodo

---
+++ script.js
@@ -0,0 +1,17 @@
+
+// typing animation script
+var typed= new Typed(".typing",{
+    strings: ["Web Developer","Trainee at AccioJob", "Freelancer"],
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true
+});
+
+var typed= new Typed(".typing-2",{
+    strings: ["Coder","Developer", "Blogger", "Designer", "Freelancer"],
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true
+});
+
+
